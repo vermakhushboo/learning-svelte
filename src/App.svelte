3f7aac0f89dcde50374360cfda9e770eaf543f8f
vermakhushboo <43381712+vermakhushboo@users.noreply.github.com>
2021-12-30 22:33:00 +0530

    Reactivity in Svelte

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let name;
+	let count = 0;
+	$: doubled = count * 2;
+
+	$: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 9;
+	}
+
+	function incrementCount() {
+		count += 1;
+	}
+
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers[numbers.length] = numbers.length + 1;
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<button on:click={incrementCount}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
+	<p>{count} doubled is {doubled}</p>
+
+	<p>{numbers.join(' + ')} = {sum}</p>
+
+	<button on:click={addNumber}>
+		Add a number
+	</button>
+</main>
+
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
